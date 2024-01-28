4bcd67783b922a9ced0c124354dd078bc54024b0
savsalmon <sav.b.everett@gmail.com>
2024-01-27 17:39:54 -0700

    that was a fake api key don't worry github

--- mailer.js
+++ mailer.js
@@ -4,8 +4,8 @@ const mailGun = require("nodemailer-mailgun-transport")
 
 const auth = {
   auth: {
-    api_key: "key-6e1d9b4f3e6e4e0d5c6d6f8c0d0e8c0a",
-    domain: "sandboxc3a5c6a0a1d14a7c8c6f4c0c4d0e8c0a.mailgun.org",
+    api_key: "#",
+    domain: "#",
   },
 }
 
